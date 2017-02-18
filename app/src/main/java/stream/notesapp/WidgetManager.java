6b97878784784b01a84697e59c9ad0bbe039632b
Ray <yangyang414414@gmail.com>
2017-02-18 11:22:14 -0600

    Heads Up Notification Widget
    *Create a widget that creates a heads up notification widget with quick reply action.

---
+++ app/src/main/java/stream/notesapp/WidgetManager.java
@@ -0,0 +1,10 @@
+package stream.notesapp;
+
+import android.os.Bundle;
+
+public class WidgetManager extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
