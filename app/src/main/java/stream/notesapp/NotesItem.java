52726f75c44b45090086542d23a29b19fde7f879
Ray <yangyang414414@gmail.com>
2017-02-18 19:58:33 -0600

    Notes Widget Creation #1
    *Create notes widget layout
    *http://stackoverflow.com/questions/2471875/processing-more-than-one-button-click-at-android-widget

---
+++ app/src/main/java/stream/notesapp/NotesItem.java
@@ -0,0 +1,19 @@
+package stream.notesapp;
+
+public class NotesItem {
+    String something;
+
+    public NotesItem(String something) {
+        super();
+        this.something = something;
+    }
+
+    public String getSomething() {
+        return something;
+    }
+
+    public void setSomething(String something) {
+        this.something = something;
+    }
+
+}
