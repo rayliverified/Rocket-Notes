30b398cfee180d73adc91ccb4d1f06f5a22f7db6
Ray <ray.li.verified@gmail.com>
2017-04-29 15:06:03 -0500

    New Note Widget Creation #2 Part 2
    *Open Edit Activity and Widget  appearance now correct.

--- app/src/main/java/stream/rocketnotes/NewNoteWidget.java
+++ app/src/main/java/stream/rocketnotes/NewNoteWidget.java
@@ -15,48 +15,33 @@ public class NewNoteWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_newnote);
 
-        // Get all ids
-        ComponentName thisWidget = new ComponentName(context,
-                NewNoteWidget.class);
-        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-        for (int widgetID : allWidgetIds) {
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.widget_newnote);
-
-            // Register an onClickListener
-            Intent intent = new Intent(context, NewNoteWidget.class);
-            intent.setAction(Constants.QUICK_NOTE);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+            //Register an onClickListener
+            Intent newNoteIntent = new Intent(context, NewNoteWidget.class);
+            newNoteIntent.setAction(Constants.QUICK_NOTE);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, newNoteIntent, 0);
             remoteViews.setOnClickPendingIntent(R.id.image_button, pendingIntent);
 
-            appWidgetManager.updateAppWidget(widgetID, remoteViews);
-
+            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
         }
-
-//        for (int i = 0; i < appWidgetIds.length; ++i) {
-//            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_newnote);
-//
-//            //Register an onClickListener
-//            Intent newNoteIntent = new Intent(context, NewNoteWidget.class);
-//            newNoteIntent.setAction(Constants.QUICK_NOTE);
-//            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, newNoteIntent, 0);
-//            remoteViews.setOnClickPendingIntent(R.id.image_button, pendingIntent);
-//
-//            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
-//        }
 
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Received", "NewNoteWidget");
         if (intent.getAction().equals(Constants.QUICK_NOTE)) {
             Log.d("onReceive", Constants.QUICK_NOTE);
-            Intent openIntent = new Intent(context, ShareActivity.class);
-            openIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(openIntent);
+            intent = new Intent(context, EditActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setAction(Constants.NEW_NOTE);
+            context.startActivity(intent);
+        }
+        else {
+            Log.d("onReceive", "Clicked");
+            super.onReceive(context, intent);
         }
     }
 }
